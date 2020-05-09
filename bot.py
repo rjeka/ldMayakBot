ebded54f2c2fc9ac8791755105b5eda4f691f726
Evgeniy Rodionov <r_jela@mail.ru>
2020-05-09 19:09:15 +0300

    new menu for help and start changed @bot.message_handler(content_types=["text"])

--- bot.py
+++ bot.py
@@ -8,29 +8,19 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, я бот ТСН "Ладожский маяк"\n'
-                                      '1. Я могу помочь узнать наличие и сумму задолженности по комунальным услугам\n'
-                                      '2  Отправлю квитанцию и QR код для оплаты коммунальных услуг\n'
-                                      '3. Покажу полезные телефоны служб поселка\n'
-                                      '4. Подскажу контакты проверенных фирм и услуг в нашем Коттеджном поселке\n\n'
-                                      'Для обзора всех возможностей введите /help')
+    bot.send_message(message.chat.id, config.START_MENU)
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
-    bot.send_message(message.chat.id, 'Привет, я бот ТСН "Ладожский маяк"\n\n'
-                                      'Введите в окно диалога или нажмите на одну из команд'
-                                      '/start - показать приветствие\n'
-                                      '/help -  показать список полезных команд\n'
-                                      '/contact - показать телефоны правления ТСН, Охраны\n'
-                                      '/bill - получить квитанцию для оплаты коммунальных услуг')
+    bot.send_message(message.chat.id, config.HELP_MENU )
 
 
 @bot.message_handler(content_types=["text"])
 def default_text(message):
     keyboard = types.InlineKeyboardMarkup()
-    url_button = types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
+    url_button = types.InlineKeyboardButton(text="Может быть поискать на Яндекс", url="https://ya.ru")
     keyboard.add(url_button)
-    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "К сожалению я не знаю такой команды.", reply_markup=keyboard)
 
 
 
