90e813a612c25831dbe94907ecd07a9c02366fab
Evgeniy Rodionov <r_jela@mail.ru>
2020-05-10 14:49:08 +0300

    adden new button and second keyboard handler

--- bot.py
+++ bot.py
@@ -5,21 +5,44 @@ from telebot import types
 bot = telebot.TeleBot(config.token)
 
 
+# -----------------------------------------Messages handlers------------------------------------------------------
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    keyboard = types.InlineKeyboardMarkup()
-    callback_button = types.InlineKeyboardButton(text="Показать все команды", callback_data="help")
-    keyboard.add(callback_button)
-    bot.send_message(message.chat.id, config.START_MENU, reply_markup=keyboard)
+    start_keyboard = types.InlineKeyboardMarkup()
+    menu_button = types.InlineKeyboardButton(text="Показать все доступные команды", callback_data="/menu")
+    start_keyboard.add(menu_button)
+    bot.send_message(message.chat.id, config.START_MENU, reply_markup=start_keyboard)
 
 
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=['help', 'menu'])
 def help_message(message):
-    bot.send_message(message.chat.id, config.HELP_MENU)
+    help_keyboard = types.InlineKeyboardMarkup(row_width=1)
+    start_button = types.InlineKeyboardButton(text="/start - показать приветствие", callback_data="/start")
+    help_button = types.InlineKeyboardButton(text="/help -  показать список всех команд", callback_data="/menu")
+    tsn_button = types.InlineKeyboardButton(text="/tsn - показать информацию о ТСН", callback_data='/tsn')
+    government_button = types.InlineKeyboardButton(text="/government - информация о госорганах",
+                                                   callback_data="/government")
+    bill_button = types.InlineKeyboardButton(text="/bill - квитанция за КУ", callback_data="/bill")
+
+    help_keyboard.add(start_button, help_button, tsn_button, government_button, bill_button)
+    bot.send_message(message.chat.id, config.HELP_MENU, reply_markup=help_keyboard)
+
+
+@bot.message_handler(commands=['tsn'])
+def tsn_message(message):
+    tsn_keyboard = types.InlineKeyboardMarkup(row_width=1)
+    requisites_button = types.InlineKeyboardButton(text="реквизиты ТСН", callback_data="/requisites")
+    tsn_keyboard.add(requisites_button)
+    bot.send_message(message.chat.id, "Выберите раздел\n", reply_markup=tsn_keyboard)
+
+
+@bot.message_handler(commands=['requisites'])
+def requisites_message(message):
+    bot.send_message(message.chat.id, config.TSN_REQUISITES)
 
 
 @bot.message_handler(commands=['bill'])
-def help_message(message):
+def bill_message(message):
     bot.send_message(message.chat.id, 'В данный момент раздел в разработке')
 
 
@@ -28,13 +51,26 @@ def default_text(message):
     keyboard = types.InlineKeyboardMarkup()
     url_button = types.InlineKeyboardButton(text="Может быть поискать на Яндекс", url="https://ya.ru")
     keyboard.add(url_button)
-    bot.send_message(message.chat.id, "К сожалению я не знаю такой команды.", reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    if call.data == "help":
-        bot.send_message(chat_id=call.message.chat.id, text=config.HELP_MENU)
+# -----------------------------------------------Keyboard handlers----------------------------------
+# main menu keyboard handler
+@bot.callback_query_handler(func=lambda call: call.data in ["/start", "/menu", "/tsn"])
+def callback_main_command(call):
+    if call.data == "/start":
+        start_message(call.message)
+    elif call.data == "/menu":
+        help_message(call.message)
+    elif call.data == "/tsn":
+        tsn_message(call.message)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in ["/requisites"])
+def callback_tsn_command(call):
+    requisites_message(call.message)
+
+
+# tsn menu keyboard handler
 
 
 if __name__ == '__main__':
