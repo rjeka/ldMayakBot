54534860c6537495113ee0e0f262eea8821611a9
Evgenii Rodionov <e.rodionov@affise.com>
2021-11-21 00:21:03 +0300

    added add_user job

--- bot.py
+++ bot.py
@@ -1,11 +1,8 @@
 import os
 import telebot
-from telebot import types
-
 import menu
-import keyboards
-
-
+import security
+import add
 
 bot = telebot.TeleBot(os.environ['TELEGRAM_TOKEN'])
 
@@ -13,31 +10,16 @@ bot = telebot.TeleBot(os.environ['TELEGRAM_TOKEN'])
 # -----------------------------------------Main menu messages handlers------------------------------
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    message_menu = menu.get_menu("SELECT text FROM menu_text WHERE name LIKE 'start'") \
-        .format(message.from_user.first_name)
-    start_keyboard = types.InlineKeyboardMarkup(row_width=1)
-    guest_button = types.InlineKeyboardButton(text="Получить код для гостевого проезда" , callback_data="/guest")
-    tsn_button = types.InlineKeyboardButton(text="Получить информацию о ТСН", callback_data="/tsn")
-    id_button = types.InlineKeyboardButton(text="Узнать свой Telegram ID", callback_data="/id")
-    start_keyboard.add(guest_button, tsn_button, id_button)
-    bot.send_message(message.chat.id, message_menu, reply_markup=start_keyboard)
-
-# help menu
-@bot.message_handler(commands=['help', 'menu'])
-def help_message(message):
-    message_menu = menu.get_menu("SELECT text FROM menu_text WHERE name LIKE 'help'")
-    help_keyboard = types.InlineKeyboardMarkup(row_width=1)
-    start_button = types.InlineKeyboardButton(text="/start - показать приветствие", callback_data="/start")
-    help_button = types.InlineKeyboardButton(text="/help -  показать список всех команд", callback_data="/menu")
-    tsn_button = types.InlineKeyboardButton(text="/tsn - показать информацию о ТСН", callback_data='/tsn')
-
-    help_keyboard.add(start_button, help_button, tsn_button)
-    bot.send_message(message.chat.id, message_menu, reply_markup=help_keyboard)
+    message_menu = menu.get_menu("SELECT text FROM menu_text WHERE name LIKE 'start'")
+    bot.send_message(message.chat.id, message_menu)
 
 
 @bot.message_handler(commands=['tsn'])
 def tsn_message(message):
-    message_menu = menu.get_menu("SELECT text FROM menu_text WHERE name LIKE 'tsn'")
+    if security.check_user_id(message.from_user.id):
+        message_menu = menu.get_menu("SELECT text FROM menu_text WHERE name LIKE 'tsn'")
+    else:
+        message_menu = menu.get_menu("SELECT text FROM menu_text WHERE name LIKE 'access_denied'")
     bot.send_message(message.chat.id, message_menu)
 
 
@@ -45,20 +27,40 @@ def tsn_message(message):
 def id_message(message):
     bot.send_message(message.chat.id, "Ваш Telegram ID: {}".format(message.from_user.id))
 
-# -----------------------------------------------Keyboard handlers----------------------------------
 
-# main menu keyboard handler
-@bot.callback_query_handler(
-    func=lambda call: call.data in ["/start", "/menu", "/tsn", "/id"])
-def callback_main_command(call):
-    if call.data == "/start":
-        start_message(call.message)
-    elif call.data == "/menu":
-        help_message(call.message)
-    elif call.data == "/tsn":
-        tsn_message(call.message)
-    elif call.data == "/id":
-        id_message(call.message)
+@bot.message_handler(commands=['admin'])
+def admin_message(message):
+    if security.check_user_id(message.from_user.id):
+        if security.check_admin_id(message.from_user.id):
+            message_menu = menu.get_menu("SELECT text FROM menu_text WHERE name LIKE 'admin'")
+        else:
+            message_menu = menu.get_menu("SELECT text FROM menu_text WHERE name LIKE 'admin_access_denied'")
+    else:
+        message_menu = menu.get_menu("SELECT text FROM menu_text WHERE name LIKE 'access_denied'")
+    bot.send_message(message.chat.id, message_menu.format(message.from_user.first_name, message.from_user.id))
+
+
+@bot.message_handler(commands=['add'])
+def add_message(message):
+    bot.send_message(message.chat.id, "Введите Telegram ID")
+
+
+@bot.message_handler(func=lambda message: message.chat.id == "Введите Telegram ID")
+def telegram_id_message(message):
+    if not message.text.isdigit():
+        bot.send_message(message.chat.id, "Вы ввели не правильный Telegram ID \nПопробуйте еще раз")
+    else:
+        bot.send_message(message.chat.id, "Telegram ID: {}".format(message.text))
+        bot.send_message(message.chat.id, "Введите номер л\с")
+
+
+@bot.message_handler(func=lambda message: message.chat.id == "Введите номер л\с")
+def account_message(message):
+    if not message.text.isdigit():
+        bot.send_message(message.chat.id, "Вы ввели не правильный номер л\с \nПопробуйте еще раз")
+    else:
+        bot.send_message(message.chat.id, "Номер л\с: {}".format(message.text))
+        bot.send_message(message.chat.id, "Пользователей создан")
 
 
 # --------------------------------------main___________________________________
